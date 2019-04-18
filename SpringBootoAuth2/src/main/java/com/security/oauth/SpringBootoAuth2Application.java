3bb227404ad4f9d064986bee9523b456d2d598e6
Kiran B S <kbs71190@gmail.com>
2019-04-18 13:23:03 +0530

    Add files via upload
    Reference : https://www.programmergate.com/spring-boot-spring-security-oauth2/
    
    To configure your application in google, follow the below steps:
        Open Google Console API
        Click on “New Project” button at the top left of the page.
        Set a name for your project then click “Create”.
        Now your application is created, choose your application from the list of projects at the top toolbar, then choose “APIs & Services” -> “Credentials” from the left menu.
        Click “Create Credentials” -> “OAuth Client Id” then choose your application type, in this tutorial we choose “Web Application”
        Add your redirect URI under “Authorized redirect URIs” text box, this is the URL which google uses when redirecting back to your application after successful authentication.
        Click on “Create”.

---
+++ SpringBootoAuth2/src/main/java/com/security/oauth/SpringBootoAuth2Application.java
@@ -0,0 +1,13 @@
+package com.security.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootoAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootoAuth2Application.class, args);
+	}
+
+}
